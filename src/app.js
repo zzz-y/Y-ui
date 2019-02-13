e229b5b07f1f0ca1d04e4918de9df83d6744c0d2
zzz-y <296345700@qq.com>
2019-02-13 17:13:35 +0800

    添加icon 和 iconRight 属性

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import Button from './button';
 
 Vue.component('y-button', Button);
-
 new Vue({
   el: '#app',
 });
--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,27 @@
 <template>
-  <button class="y-button">按钮</button>
+  <button class="y-button" :class="[iconRight?'icon-right':'']">
+    <svg class="icon" v-if="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
   export default {
     name: 'button',
+    props: {
+      icon: {},
+      iconRight: {
+        type: Boolean,
+        default: false,
+        validator(value) {
+
+        },
+      }
+    }
   }
 </script>
 
@@ -17,14 +34,42 @@
     border-radius: var(--border-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
+
     &:hover {
       border-color: var(--border-color-hover);
     }
+
     &:active {
       border-color: var(--button-active-bg);
     }
+
     &:focus {
       outline: none;
+    }
+
+    > .icon {
+      order: 1;
+      margin-right: .3em;
+    }
+
+    > .content {
+      order: 2;
+    }
+
+    &.icon-right {
+      > .icon {
+        order: 2;
+        margin-right: 0;
+        margin-left: .3em;
+      }
+
+      > .content {
+        order: 1;
+      }
     }
   }
 </style>
