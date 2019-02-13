3d9b136b4b49c257aa4c3431b9bd14201e4273a9
zzz-y <296345700@qq.com>
2019-02-13 09:13:48 +0800

    添加单文件组件 button

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Y-ui
+
+## 安装
+
+使用本框架前请在css中开启border-box
+```
+*{
+  box-sizing:border-box
+}
+```
---
+++ src/button.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="y-button">按钮</button>
+</template>
+
+<script>
+  export default {
+    name: 'button',
+  }
+</script>
+
+<style lang="scss">
+
+  .y-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    &:hover {
+      border-color: var(--border-color-hover);
+    }
+    &:active {
+      border-color: var(--button-active-bg);
+    }
+    &:focus {
+      outline: none;
+    }
+  }
+</style>
