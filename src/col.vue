66c800fbb4d265ad3decd467b75d5d94e34f3cda
zzz-y <zhangyue0-0@qq.com>
2019-02-19 16:47:54 +0800

    基本实现 row 和 col

---
+++ src/col.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="col" :class="colClasses" :style="colStyle">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'y-col',
+    props: {
+      span: {
+        type: [Number, String],
+      },
+      offset: {
+        type: [Number, String],
+      },
+    },
+    data() {
+      return {
+        gutter: 0,
+      }
+    },
+    computed: {
+      colClasses() {
+        let {span, offset} = this;
+        return [
+          span && `col-${span}`,
+          offset && `offset-${offset}`
+        ]
+      },
+      colStyle() {
+        return {
+          paddingLeft: `${this.gutter / 2}px`,
+          paddingRight: `${this.gutter / 2}px`
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .col {
+    height: 30px;
+    width: 50%;
+
+    $class-prefix: col-;
+
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+
+    $class-prefix: offset-;
+
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+</style>
---
+++ src/row.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="row" :style="rowStyle">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'y-row',
+    props: {
+      gutter: {
+        type: [Number, String],
+        default: 0,
+      }
+    },
+    mounted() {
+      this.$children.forEach((vm) => {
+        vm.gutter = this.gutter;
+      })
+    },
+    computed: {
+      rowStyle() {
+        return {
+          marginLeft: `${-this.gutter / 2}px`,
+          marginRight: `${-this.gutter / 2}px`
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .row {
+    display: flex;
+  }
+</style>
