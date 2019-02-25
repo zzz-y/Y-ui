5998acdd981dc91671cab3661810bd739565f385
zzz-y <zhangyue0-0@qq.com>
2019-02-25 17:20:17 +0800

    添加布局组件

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ import ButtonGroup from './button-group';
 import Input from './input';
 import Row from './row';
 import Col from './col';
+import Layout from './layout';
+import Header from './header';
+import Sider from './sider';
+import Content from './content';
+import Footer from './footer';
 
 Vue.component('y-button', Button);
 Vue.component('y-icon', Icon);
@@ -12,6 +17,11 @@ Vue.component('y-button-group', ButtonGroup);
 Vue.component('y-input', Input);
 Vue.component('y-row', Row);
 Vue.component('y-col', Col);
+Vue.component('y-layout', Layout);
+Vue.component('y-header', Header);
+Vue.component('y-sider', Sider);
+Vue.component('y-content', Content);
+Vue.component('y-footer', Footer);
 
 new Vue({
   el: '#app',
---
+++ src/header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'y-header'
+  }
+</script>
+
+<style lang="scss" scoped>
+  .header{
+
+  }
+</style>
